8f1dde3f6ef32b203a1da9edcf87da898177cd5b
Ivan Cipriani <ivan.cipriani@eaxct.com>
2022-04-27 15:31:17 +0200

    playwright small test example

--- PlaywrightFramework/playwright.config.ts
+++ PlaywrightFramework/playwright.config.ts
@@ -1,8 +1,10 @@
 import { PlaywrightTestConfig } from "@playwright/test";
+
 const config: PlaywrightTestConfig = {
   use: {
     headless: false,
     baseURL: "https://localhost:44345",
   },
 };
+
 export default config;
